dbc046fcc0539563e4d82f5e80945763707ae92d
javenleeCH <javenlee@foxmail.com>
2024-08-24 12:19:53 +0000

    init

--- sum.test.ts
+++ sum.test.ts
@@ -1,7 +1,13 @@
 // sum.test.js
 import { expect, test } from 'vitest'
-import { sum } from './sum.js'
+import { Peng, sum } from './sum'
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3)
+})
+
+test('peng test', () => {
+  let peng = new Peng('da peng');
+  // @ts-ignore
+  expect(peng.getName()).toBe('da peng');
 })
